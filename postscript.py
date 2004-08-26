532f44048501e3a6ecdb3b633d1d57567b7e44f2
Jamie Wilkinson <jamie.wilkinson@anchor.com.au>
2004-08-26 06:08:01 +0000

    evilise function calls by 81%

--- postscript.py
+++ postscript.py
@@ -7,18 +7,30 @@ class PostScript:
 	def __init__(self):
 		self.o = []
 		self.indent = 0
-
+		self.functions = ["moveto",
+						  "lineto",
+						  "rlineto",
+						  "newpath",
+						  "closepath",
+						  "stroke",
+						  "fill",
+						  "clip",
+						  "showpage",
+						  "findfont",
+						  "scalefont",
+						  "setfont",
+						  "show",
+						  "rotate",
+						  "translate",
+						  "scale",
+						  "charpath",
+						  "setgray",
+						  "setlinewidth"
+						  ]
+						  
+						  
 	def append(self, command):
 		self.o.append(" " * self.indent + command)
-
-	def moveto(self, x, y):
-		self.append("%s %s moveto" % (x, y))
-
-	def lineto(self, x, y):
-		self.append("%s %s lineto" % (x, y))
-
-	def rlineto(self, x, y):
-		self.append("%s %s rlineto" % (x, y))
 
 	def gsave(self):
 		self.append("gsave")
@@ -27,54 +39,6 @@ class PostScript:
 	def grestore(self):
 		self.indent -= 2
 		self.append("grestore")
-
-	def newpath(self):
-		self.append("newpath")
-
-	def closepath(self):
-		self.append("closepath")
-
-	def stroke(self):
-		self.append("stroke")
-
-	def fill(self):
-		self.append("fill")
-
-	def showpage(self):
-		self.append("showpage")
-
-	def findfont(self, font):
-		self.append("/%s findfont" % (font,))
-
-	def scalefont(self, scale):
-		self.append("%s scalefont" % (scale,))
-
-	def setfont(self):
-		self.append("setfont")
-
-	def show(self, text):
-		self.append("(%s) show" % (text,))
-
-	def rotate(self, ang):
-		self.append("%s rotate" % (ang,))
-
-	def translate(self, x, y):
-		self.append("%s %s translate" % (x, y))
-
-	def scale(self, x, y):
-		self.append("%s %s scale" % (x, y))
-
-	def clip(self):
-		self.append("clip")
-
-	def charpath(self, arg):
-		self.append("%s charpath" % (arg,))
-
-	def setgray(self, gray):
-		self.append("%s setgray" % (gray,))
-
-	def setlinewidth(self, linewidth):
-		self.append("%s setlinewidth" % (linewidth,))
 
 	def comment(self, comment):
 		self.append("%% %s" % (comment,))
@@ -85,15 +49,28 @@ class PostScript:
 			output += line + "\n"
 		return output
 
+	def quote(self, arg):
+		"""quote the argument from expansion"""
+		return "/" + arg
+
+	def command(self, cmd, *args):
+		self.append((("%s " * len(args)) % args) + cmd)
+
+	def __getattr__(self, cmd):
+		if cmd not in self.functions:
+			raise AttributeError, "PostScript instance has no attribute '%s'" % (cmd,)
+		r = lambda *args: self.command(cmd, *args)
+		return r
+
 if __name__ == '__main__':
 	ps = PostScript()
 	ps.comment("sample of printing text")
-	ps.findfont("Times-Roman")
+	ps.findfont(ps.quote("Times-Roman"))
 	ps.scalefont(20)
 	ps.setfont()
 	ps.gsave()
 	ps.newpath()
 	ps.moveto(72, 72)
-	ps.show("Hello, world!")
+	ps.show("(Hello, world!)")
 	ps.grestore()
 	print ps.render()
